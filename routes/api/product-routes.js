edc95e51286a99ce5a843fc19d1aae95412d469f
Sajjadalgburi <dfgsajjad@gmail.com>
2024-02-21 14:17:45 -0700

    Created a GET method to find all products within product model

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,22 +1,29 @@
-const router = require('express').Router();
-const { Product, Category, Tag, ProductTag } = require('../../models');
+const router = require("express").Router();
+const { Product, Category, Tag, ProductTag } = require("../../models");
 
 // The `/api/products` endpoint
 
-// get all products
-router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+// GET method to get everything from the Product model
+router.get("/", async (req, res) => {
+  try {
+    // Fetch all products from the database including associated tags and categories
+    const productData = await Product.findAll({
+      include: [{ model: Tag }, { model: Category }],
+    });
+    // Send a 200 OK response along with the product data
+    res.status(200).json(productData);
+  } catch (err) {
+    console.error(err);
+    // If an error occurs, log the error and send a 500 Internal Server Error response along with the error message
+    res.status(500).send(err);
+  }
 });
 
-// get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
-});
+// GET method for a sepcific Product id
+router.get("/:id", async (req, res) => {});
 
 // create new product
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -48,7 +55,7 @@ router.post('/', (req, res) => {
 });
 
 // update product
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // update product data
   Product.update(req.body, {
     where: {
@@ -57,9 +64,8 @@ router.put('/:id', (req, res) => {
   })
     .then((product) => {
       if (req.body.tagIds && req.body.tagIds.length) {
-
         ProductTag.findAll({
-          where: { product_id: req.params.id }
+          where: { product_id: req.params.id },
         }).then((productTags) => {
           // create filtered list of new tag_ids
           const productTagIds = productTags.map(({ tag_id }) => tag_id);
@@ -92,7 +98,7 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // delete one product by its `id` value
 });
 
