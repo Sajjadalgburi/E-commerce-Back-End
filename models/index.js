7c41d4ffaa9f289352c91812a3868a63bc91a212
Sajjadalgburi <dfgsajjad@gmail.com>
2024-02-21 13:33:38 -0700

    Created the associations through sequelize standards

--- models/index.js
+++ models/index.js
@@ -1,17 +1,37 @@
-// import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+// Import necessary models
+const Product = require("./Product"); // Import Product model
+const Category = require("./Category"); // Import Category model
+const Tag = require("./Tag"); // Import Tag model
+const ProductTag = require("./ProductTag"); // Import ProductTag model
 
-// Products belongsTo Category
+// Relation Set 1: Category - Product relationship
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+  onDelete: "CASCADE", // If a Category is deleted, all associated Product instances should be deleted as well.
+});
 
-// Products belongToMany Tags (through ProductTag)
+// Products belong to a Category
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+});
 
-// Tags belongToMany Products (through ProductTag)
+// Relation Set 2: Product - Tag relationship
 
+// Products belong to many Tags through ProductTag
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: "product_id",
+});
+
+// Tags belong to many Products through ProductTag
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: "tag_id",
+});
+
+// Export models
 module.exports = {
   Product,
   Category,
