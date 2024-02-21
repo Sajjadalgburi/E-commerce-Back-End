6e223a659bb4fd211d597ad4be343b263dec4869
Sajjadalgburi <dfgsajjad@gmail.com>
2024-02-21 12:50:28 -0700

    defined the columns for the ProductTag model

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,20 +1,46 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 
+// Defining the ProductTag class which extends Model
 class ProductTag extends Model {}
 
+// Initializing the ProductTag model with attributes and options
 ProductTag.init(
   {
-    // define columns
+    // ID attribute
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    // Product ID attribute with reference to the Product model
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "product",
+        key: "id",
+      },
+    },
+    // Tag ID attribute with reference to the Tag model
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "tag",
+        key: "id",
+      },
+    },
   },
   {
+    // Sequelize connection
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product_tag',
+    modelName: "product_tag",
   }
 );
 
+// Exporting the ProductTag model
 module.exports = ProductTag;
