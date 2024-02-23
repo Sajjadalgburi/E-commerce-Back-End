5adf0369c3cac42a8cc50b46a63de05f7045bd9d
Sajjadalgburi <dfgsajjad@gmail.com>
2024-02-23 14:33:40 -0700

    Created a POST method for user to post a product

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -36,36 +36,39 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-// create new product
-router.post("/", (req, res) => {
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
-  Product.create(req.body)
-    .then((product) => {
-      // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-      if (req.body.tagIds.length) {
-        const productTagIdArr = req.body.tagIds.map((tag_id) => {
-          return {
-            product_id: product.id,
-            tag_id,
-          };
-        });
-        return ProductTag.bulkCreate(productTagIdArr);
-      }
-      // if no product tags, just respond
-      res.status(200).json(product);
-    })
-    .then((productTagIds) => res.status(200).json(productTagIds))
-    .catch((err) => {
-      console.log(err);
-      res.status(400).json(err);
+// POST method for user to post a product
+router.post("/", async (req, res) => {
+  try {
+    const { product_name, price, stock, tagIds } = req.body;
+
+    // Create the product
+    const product = await Product.create({
+      product_name,
+      price,
+      stock,
     });
+
+    // Associate tags with the product using ProductTag
+    if (tagIds && tagIds.length > 0) {
+      const tags = await Tag.findAll({
+        where: {
+          id: tagIds,
+        },
+      });
+
+      await product.addTags(tags);
+    }
+
+    // Fetch the created product with associated tags
+    const productWithTags = await Product.findByPk(product.id, {
+      include: Tag,
+    });
+
+    res.status(200).json(productWithTags);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send(err);
+  }
 });
 
 // update product
