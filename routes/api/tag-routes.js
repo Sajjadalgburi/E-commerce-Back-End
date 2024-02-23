7cd146f01dcf247fa2556eca77f8f28288e8d59b
Sajjadalgburi <dfgsajjad@gmail.com>
2024-02-23 14:59:29 -0700

    Defined a route handler for GET requests to the root endpoint

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,27 +1,39 @@
-const router = require('express').Router();
-const { Tag, Product, ProductTag } = require('../../models');
+const router = require("express").Router();
+const { Tag, Product, ProductTag } = require("../../models");
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+// Define a route handler for GET requests to the root endpoint
+router.get("/", async (req, res) => {
+  try {
+    // Attempt to retrieve all tag data from the database, including associated products
+    const tagData = await Tag.findAll({
+      include: [{ model: Product }],
+    });
+    // If successful, send a 200 OK response with the tag data in JSON format
+    res.status(200).json(tagData);
+  } catch (err) {
+    // If an error occurs during the database query, log the error to the console
+    console.error(err);
+    // Send a 500 Internal Server Error response along with the error message
+    res.status(500).send(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
 });
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   // create a new tag
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // update a tag's name by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // delete on tag by its `id` value
 });
 
